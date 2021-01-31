71027a32c610319d5533233ed76a8038577e4082
Cathy-B <56276886+Cathy-B@users.noreply.github.com>
2021-01-31 13:52:26 -0300

    actualizacion

--- ModuloV_Dia9/src/main/java/cl/awakelab/grupal10/modelo/CapacitacionRepositorio.java
+++
@@ -1,9 +0,0 @@
-package cl.awakelab.grupal10.modelo;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CapacitacionRepositorio extends CrudRepository<Capacitacion, Integer>{
-
-}
--- ModuloV_Dia9/src/main/java/cl/awakelab/grupal10/servicio/ContactoServicio.java
+++
@@ -1,8 +0,0 @@
-package cl.awakelab.grupal10.servicio;
-
-import cl.awakelab.grupal10.modelo.Contacto;
-
-public interface ContactoServicio {
-
-	public boolean crearContacto(Contacto contacto);
-}
